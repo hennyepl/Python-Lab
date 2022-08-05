9adb1e93e2cf8d6736edfecbc8d8e4c7a396587a
Henok Lamiso <epapalink68@gmail.com>
2022-08-04 19:33:12 -0600

    evenODD

--- Intoduction/main.py
+++ Intoduction/main.py
@@ -1,1 +1,9 @@
 print("Welcome Henny to the world of Python")
+# read number from user and print it
+
+print("Welcome Henny to the world of Python")
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
--- Object and Class/main.py
+++
@@ -1,8 +0,0 @@
-# read number from user and print it
-
-print("Welcome Henny to the world of Python")
-number = int(input("Enter a number: "))
-if number % 2 == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
