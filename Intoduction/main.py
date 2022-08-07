4d9b3f20c6897a033660e34903161c60cb6fb597
Henok Lamiso <epapalink68@gmail.com>
2022-08-06 18:49:41 -0600

    importing file

--- Intoduction/main.py
+++ Intoduction/main.py
@@ -1,5 +1,4 @@
-number_of_student = int(input("Enter number of students: "))
-name_of_student = []
-for i in range(number_of_student):
-    name_of_student[i] = input("Enter name of student: ")
-print(name_of_student)
+import constant
+
+print (constant.PI)
+print (constant.Gravity)
