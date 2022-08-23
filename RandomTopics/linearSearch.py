6b1be0974530346b6ed14a13f94d45002f9840b4
Henok Lamiso <epapalink68@gmail.com>
2022-08-22 18:59:36 -0600

    Linear Search in Python

---
+++ RandomTopics/linearSearch.py
@@ -0,0 +1,24 @@
+# This program read n numbers from the user and then search for a number in the list.
+
+import random
+
+def linearSearch(list, n):
+    for i in range(len(list)):
+        if list[i] == n:
+            return True
+    return False
+
+
+def main():
+    n = int(input("Enter the number of elements in the list: "))
+    list = []
+    for i in range(n):
+        list.append(random.randint(1, 100))
+    print(list)
+    n = int(input("Enter the number to search: "))
+    if linearSearch(list, n):
+        print("Found")
+    else:
+        print("Not found")
+
+
