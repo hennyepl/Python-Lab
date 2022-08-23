228384cad9afe005ba10090ad81bc608edc3d33b
Henok Lamiso <epapalink68@gmail.com>
2022-08-22 19:12:19 -0600

    unit test

--- RandomTopics/linearSearch.py
+++ RandomTopics/linearSearch.py
@@ -1,6 +1,7 @@
 # This program read n numbers from the user and then search for a number in the list.
 
 import random
+import unittest
 
 def linearSearch(list, n):
     for i in range(len(list)):
@@ -24,4 +25,44 @@ def main():
 # print out
 if __name__ == "__main__":
     main()
-    
+
+
+# unit test
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# n = 5
+# 
+
+class TestLinearSearch(unittest.TestCase):
+    def test_linearSearch(self):
+        list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        n = 5
+        self.assertEqual(linearSearch(list, n), True)
+        n = 11
+        self.assertEqual(linearSearch(list, n), False)
+        n = 0
+        self.assertEqual(linearSearch(list, n), False)
+        n = -1
+        self.assertEqual(linearSearch(list, n), False)
+        n = 100
+        self.assertEqual(linearSearch(list, n), False)
+        n = -100
+        self.assertEqual(linearSearch(list, n), False)
+        n = 0.1
+        self.assertEqual(linearSearch(list, n), False)
+        n = -0.1
+        self.assertEqual(linearSearch(list, n), False)
+        n = 1.1
+        self.assertEqual(linearSearch(list, n), False)
+        n = -1.1
+        self.assertEqual(linearSearch(list, n), False)
+        n = "1"
+        self.assertEqual(linearSearch(list, n), False)
+        n = "1.1"
+        self.assertEqual(linearSearch(list, n), False)
+        n = "1.1.1"
+        self.assertEqual(linearSearch(list, n), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # main()
